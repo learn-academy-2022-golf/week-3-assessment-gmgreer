ef853aafcc547e4a57130df6eb534adfac645cac
gmgreer <beauski8@gmail.com>
2022-11-14 08:31:01 -0800

    all done

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,9 @@
 // ASSESSMENT 3: Coding Practical Questions with Jest
 
+
+
+
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -14,54 +18,181 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const fibLength1 = 6
-// Expected output: [1, 1, 2, 3, 5, 8]
+describe("fib", ()=>{
+  it("function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.", ()=>{
+    const fibLength1 = 6
+    const output1 = [1, 1, 2, 3, 5, 8]
 
-const fibLength2 = 10
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+    const fibLength2 = 10
+    const output2 = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
-// b) Create the function that makes the test pass.
+    expect(fib(fibLength1)).toEqual(output1)
+    expect(fib(fibLength2)).toEqual(output2)
+
+  })
+})
+
+describe("fibRec", ()=>{
+  it("function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.", ()=>{
+    const fibLength1 = 6
+    const output1 = [1, 1, 2, 3, 5, 8]
+
+    const fibLength2 = 10
+    const output2 = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+    expect(fibRec(fibLength1)).toEqual(output1)
+    expect(fibRec(fibLength2)).toEqual(output2)
+
+  })
+})
+
+
+// b) Create the function that makes the test pass
+// create a function that takes in a number as an argument
+// return an array of length of the number with the fibonacci sequence. The fibonacci sequence is just the sum of the previous two numbers.
+// there will need to be a loop that sums the previous two numbers in an array then place that summed number at the end of the array. The array should loop until the length of the array reaches the function argument.
+//since the number will never be less than or equal to two the first three numbers in the fibonacci array will always be [1,1,2]. This give us a place to start and start adding up numbers based on their index.
+//after I began writing I decided to go with a while loop to decide when to stop. I just need to return the sum of the last two numbers in the array as the last number in the array
+
+//recursion
+//Out of practice with recursion so I am going to try it.
+//Using recursion I will set up a base condition to stop the recursion and then just call the function again and again until I meat that condition
+//This was difficult, I understood how to get a sequence of numbers but putting it into an array was really difficult.
+// The condition is that the number won't be 2 or less so we can set the initial condition as n < 3 and return the array we are supposed to get so this will be the initial array created when we return from the top of the stack
+//the stack is a concept in recursion, each time we go into the return statement past the base case we are calling fibRec again so it isn't able to complete its return and it will place the result on top of a stack.
+//It will keep doing this until the base condition of num < 3 so when num = 2 we will set the arr to [1,1]. After this it will call the stack in order so we will keep adding the next sum to the array.
+//The current fibRec function is the third refactoring, I was trying to get it in one line of code. The explanation above should still be accurate I am just using ternary operator to smoosh it down. 
+
+
+const fib = (num) => {
+  let fibArr = [1,1,2]
+  while (fibArr.length < num){
+    fibArr.push((fibArr[fibArr.length -1]) + (fibArr[fibArr.length - 2]))
+  }
+  return fibArr
+ }
+
+
+ const fibRec = (num) => {
+  return num < 3 ? [1,1] : [...fibRec(num-1), fibRec(num-1)[num-2]
+   + fibRec(num-1)[num-3]]
+ }
+  
+
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+
+// result after coming back and doing the recursion
+// Test Suites: 1 passed, 1 total
+// Tests:       5 passed, 5 total
+
 
 // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const studyMinutesWeek1 = {
-  sunday: 90,
-  monday: 30,
-  tuesday: 20,
-  wednesday: 15,
-  thursday: 30,
-  friday: 15,
-  saturday: 60
-}
-// Expected output: [15, 15, 20, 30, 30, 60, 90]
+describe("orderedArray", () => {
+  it("unction that takes in an object and returns an array of the values sorted from least to greatest.", ()=> {
 
-const studyMinutesWeek2 = {
-  sunday: 100,
-  monday: 10,
-  tuesday: 45,
-  wednesday: 60,
-  thursday: 20,
-  friday: 15,
-  saturday: 65
-}
-// Expected output: [10, 15, 20, 45, 60, 65, 100]
+    const studyMinutesWeek1 = {
+      sunday: 90,
+      monday: 30,
+      tuesday: 20,
+      wednesday: 15,
+      thursday: 30,
+      friday: 15,
+      saturday: 60
+    }
+    const output =  [15, 15, 20, 30, 30, 60, 90]
+
+    expect(orderedArray(studyMinutesWeek1)).toEqual(output)
+  })
+})
+
+describe("orderedArrayForIn", () => {
+  it("function that takes in an object and returns an array of the values sorted from least to greatest.", ()=> {
+
+    const studyMinutesWeek1 = {
+      sunday: 90,
+      monday: 30,
+      tuesday: 20,
+      wednesday: 15,
+      thursday: 30,
+      friday: 15,
+      saturday: 60
+    }
+    const output =  [15, 15, 20, 30, 30, 60, 90]
+
+    expect(orderedArrayForIn(studyMinutesWeek1)).toEqual(output)
+  })
+})
 
 // b) Create the function that makes the test pass.
+// create a function that takes an object as an argument
+// the function will return an array of the objects values ordered from least to greatest
+// Will use the suggested Object.value() to get the values of the object into an array, otherwise could have done a for..in loop to extract only the values. May try that as well for practice.
+// after we extract the array of only values can simply sort the array with .sort(a,b => a-b)
+//i thought that the for in loop obj.x would work but it needs to be in bracket notation.
+
+const orderedArray = (obj) => {
+   return Object.values(obj).sort((a,b) => a-b)
+}
+
+const orderedArrayForIn = (obj) => {
+  let arr = []
+  for (let x in obj) {
+    arr.push(obj[x])
+  }
+  return arr.sort((a,b)=> a-b)
+}
+
+// Test Suites: 1 passed, 1 total
+// Tests:       3 passed, 3 total
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const accountTransactions1 = [100, -17, -23, -9]
-// Expected output: [100, 83, 60, 51]
+describe("sumArray", ()=> {
+  it("function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.", ()=> {
+    const accountTransactions1 = [100, -17, -23, -9]
+    let output1 = [100, 83, 60, 51]
 
-const accountTransactions2 = [250, -89, 100, -96]
-// Expected output: [250, 161, 261, 165]
+    const accountTransactions2 = [250, -89, 100, -96]
+    let output2 = [250, 161, 261, 165]
 
-const accountTransactions3 = []
-// Expected output: []
+    const accountTransactions3 = []
+    let output3 = []
+
+    expect(sumArray(accountTransactions1)).toEqual(output1)
+    expect(sumArray(accountTransactions2)).toEqual(output2)
+    expect(sumArray(accountTransactions3)).toEqual(output3)
+
+
+  })
+})
+
+
 
 // b) Create the function that makes the test pass.
+// create a function that takes an array as an argument
+// the function will return an array that sums with the previous number in the array replacing the index with the new sum then moves on to the next index and repeats so that the array is of equal length as the previous array.
+// if given an empty array should return an empty array.
+// We must loop through the array while also saving the new sum to be used on the next value
+// I believe I can create a variable to keep track of that new sum and push that new sum into a new array.
+
+
+const sumArray = (arr) => {
+  let newArr = []
+  let sum = 0
+  for (let i = 0; i < arr.length; i++) {  
+    sum += arr[i]
+    newArr.push(sum)
+    }
+    return newArr
+}
+
+// Test Suites: 1 passed, 1 total
+// Tests:       4 passed, 4 total
--- interview-questions.md
+++ interview-questions.md
@@ -8,48 +8,71 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
-Your answer:
+Your answer: I think of jsx as a way to use javascript logic within html or maybe a way to use html inside javascript. There are several reserved names in javascript such as for and class therefore in JSX you must use htmlFor and className respectively. There are a few others that I will include in my researched answer.
 
-Researched answer:
+Researched answer: JSX is a syntax extension to javascript. JSX solves the problem of having your rendering or UI logic separate from the actual html. You can now have javascript directly influence the rendering in the same place by separating things out in components and putting logic in simple curly bracers.
+
+Some other differences is that JSX uses camel casing when specifying attributes. So class becomes className and tabindex becomes tabIndex. This is the case for all attributes. Other examples are onclick becomes onClick and onmouseover becomes onMouseOver.
+
+All tags self close in JSX even divs and spans. You wouldn't want to do that but it is still possible. You must use the /> syntax to self close though.
+
+Inline styles are written as objects inside the style tag. For example:
+style={{color: "red"}}
 
 2. What is yarn? What file(s) are modified in a React application when you run the command yarn in your terminal?
 
-Your answer:
+Your answer: Yarn is a packet manager just like npm which is node packet manager. Yarn adds node modules and also modifies package.json. The node modules are packets or snippets of code that help build out a project and gives the developer a lot of extra tools.
 
-Researched answer:
+
+
+Researched answer: Yarn allows for people to upload packages that other people can use quickly and easily straight from the terminal just by typing in yarn install bla bla bla. Then you can use some really cool pieces of prebuilt code. 
+
+The files that are edited are the node_modules based on what is in your package.json and also yarn.lock. Yarn.lock tries to satisfy all the dependencies listed in package.json files.
 
 3. What is an anonymous function in JavaScript?
 
-Your answer:
+Your answer: An anonymous function is a function syntax that looks like ()=>{} which is often used in call back functions such as in .map .filter and so on. I just recently watched a video about the limitations of anonymous functions. My understanding is that it does not include the this that would live in a normal function call such as function name(){}. This means you shouldn't use an anonymous function to create a class, factory function, or maybe instantiate an object. 
 
-Researched answer:
+Researched answer: Anonymous functions are functions without a name. Of course you can assign the function to a variable if you need to call it later but if you do not do that you can not call it anywhere else. An anonymous function not assigned to a variable must be enclosed in parentheses.
+
+Anonymous functions can also be used as arguments in other functions. THis is how .map, .filter, and higher order functions work in general.
+
+Something neat is you can immediately call an anonymous function by putting parentheses after it (()=>{})().
+
+The major limitation of anonymous functions is that they can not be reused.
 
 4. What is the difference between state values and props in React?
 
-Your answer:
+Your answer: State values are values tracked by react in order to decide when the DOM needs to be updated. We put variables in state that we want the user to influence. Things such as form data or text inputs. Maybe we put a toggle here so a modal will show up or go away. State can also hold important data we get back from an api so that we know to update the page when new information comes in.
 
-Researched answer:
+Props are informational packets or functions we want our components to use. In this way if we are passing some state information through props the component will update on the DOM when state changes.
+
+Researched answer: Props get passed to a component similar to a function parameter. State is something that is managed within a function. Whenever props or state are changed a rerendering occurs.
 
 5. What is the DOM?
 
-Your answer:
+Your answer: The DOM is the document object model. This is what the browser displays. If something changes on the DOM the page must be updated. This is where react becomes so useful as it is able to only update specific parts of the DOM that are changed.
 
-Researched answer:
+Researched answer: The DOM is a programming interface that represents the page in a way that it can be manipulated. It does this by representing the document as nodes and objects. In this way the programmer can access specific parts of the document and then manipulate them.
+
+The virtual DOM or VDOM in react is a virtual representation of the DOM kept in memory. The VDOM is then synced up with the real DOM and any changes in the virtual DOM can then be reconciled with the actual DOM. So in React all you are doing is manipulating this VDOM then react behind the scenes does the work to make sure only the part of the VDOM that you changed gets updated in the real DOM creating a smoother user experience.
 
 6. STRETCH: Which is the difference between a div and a span?
 
-Your answer:
+Your answer: I am not sure. Both are html tags. I think div might create a new node in the document object model while a span will be included within the current node.
 
-Researched answer:
+I do know that you can create a span in the same line as other html tags and it will stay within that line. That is what made me thing about the above answer. If I put a new div it would create something on a new line while a span will keep the information on the same line.
+
+Researched answer: I was way off in that node idea I think. But the difference is div is a block level element and span is an inline level element. Divs create a knew block to encapsulate information while Spans create a new inline element to encapsulate information. Divs take up the entire length of their block. The span tag is used to separate out elements on the same line for styling purposes. Like for a group of words in a paragraph. 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Object-oriented programming:
+1. Object-oriented programming: Object oriented programming is the practice of storing all data and methods within objects. This style of programming will often use classes as the basis for developing their structure. object oriented programming allows for inheritance and the ability to quickly and easily pass stored information through different classes. Because a lot of the information is abstracted away in different classes it can be difficult to read and follow what is going on.
 
-2. Ruby:
+2. Ruby: Ruby is a dynamic open source programming language. In ruby everything is an object. This allows for every piece of code to have attached methods, even integers.
 
-3. Implicit return:
+3. Implicit return: An implicit return is a return from a function without using the word return. Ruby has implicit returns on all functions. Ruby will implicitly return the last line of code within a function.
 
-4. Ruby blocks:
+4. Ruby blocks: Ruby blocks are block of code that end in end. These can block in functions or loops or anything you can think of. It is basically a function but since everything is an object we will call them methods.
 
-5. Ruby hashes:
+5. Ruby hashes: Ruby hashes are objects. They have key value pairs and can be iterated over like arrays but with specified keys rather than indexed. 
